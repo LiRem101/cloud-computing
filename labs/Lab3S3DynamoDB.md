e74671e65ce4bbc708325382ae93e049b0a68745
Lian Remme <lirem101@hhu.de>
2022-08-17 15:49:31 +0800

    Wrote code to save to s3 bucket

--- labs/Lab3S3DynamoDB.md
+++ labs/Lab3S3DynamoDB.md
@@ -53,6 +53,59 @@ NOTE the easiest way to upload files is to use the command:
 s3.upload_file()
 ```
 
+```
+import os
+import boto3
+import botocore.exceptions
+import base64
+import sys
+import credentials as cred
+
+ROOT_DIR = '.'
+ROOT_S3_DIR = str(cred.STUD_NR) + '-cloudstorage'
+
+def upload_file(folder_name, file, file_name):
+    print("Uploading %s" % file)
+    try:
+        response = s3.upload_file(Filename=file, Bucket=ROOT_S3_DIR, 
+                                  Key="/%s%s" % (folder_name, file_name))
+        print(response)
+    except Exception as error:
+        print("Error: " + str(error))
+
+
+def create_bucket(bucket_config: dict):
+    try:
+        response = s3.create_bucket(Bucket=ROOT_S3_DIR, CreateBucketConfiguration=bucket_config)
+    except botocore.exceptions.ClientError as error:
+        if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            response = "Bucket already existed."
+        else:
+            raise error
+    print(response)
+
+# parse directory and upload files
+def upload_files():
+    for dir_name, subdir_list, file_list in os.walk(ROOT_DIR, topdown=True):
+        if dir_name != ROOT_DIR and not dir_name.startswith("./__"):
+            for fname in file_list:
+                print("%s/" % dir_name[2:] + " " +"%s/%s" % (dir_name, fname) + " " + fname)
+                upload_file("%s/" % dir_name[2:], "%s/%s" % (dir_name, fname), fname)
+
+    print("done")
+
+
+if __name__ == '__main__':
+    s3 = boto3.client("s3")
+    bucket_config = {'LocationConstraint': 'ap-southeast-2'}
+    if('-i' in sys.argv or '--initialise=True' in sys.argv):
+        create_bucket(bucket_config)
+        upload_files()
+```
+
+![Results of the Python program.](images/lab3_s3_save.png)
+
+
 ### [Step 3] Restore from S3
 
 Create a new program called restorefromcloud.py that reads the S3 bucket and writes the contents of the bucket within the appropriate directories. You should have a copy of the files and the directories you started with.
--- labs/code/lab03/cloudstorage.py
+++ labs/code/lab03/cloudstorage.py
@@ -1,50 +1,49 @@
-
 import os
 import boto3
+import botocore.exceptions
 import base64
-
-# ------------------------------
-# CITS5503
-#
-# cloudstorage.py
-#
-# skeleton application to copy local files to S3
-#
-# Given a root local directory, will return files in each level and
-# copy to same path on S3
-#
-# ------------------------------ 
-
+import sys
+import credentials as cred
 
 ROOT_DIR = '.'
-ROOT_S3_DIR = 'STUDENTNUMBER-cloudstorage'
-
-
-s3 = boto3.client("s3")
-
-bucket_config = {'LocationConstraint': 'ap-southeast-2'}
+ROOT_S3_DIR = str(cred.STUD_NR) + '-cloudstorage'
 
 def upload_file(folder_name, file, file_name):
-
     print("Uploading %s" % file)
+    try:
+        response = s3.upload_file(Filename=file, Bucket=ROOT_S3_DIR, Key="/%s%s" % (folder_name, file_name))
+        print(response)
+    except Exception as error:
+        print("Error: " + str(error))
 
 
-# Main program
-# Insert code to create bucket if not there
-
-try:
-
+def create_bucket(bucket_config: dict):
+    try:
+        response = s3.create_bucket(Bucket=ROOT_S3_DIR, CreateBucketConfiguration=bucket_config)
+    except botocore.exceptions.ClientError as error:
+        if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            response = "Bucket already existed."
+        else:
+            raise error
     print(response)
-except Exception as error:
-    pass
-
 
 # parse directory and upload files
+def upload_files():
+    for dir_name, subdir_list, file_list in os.walk(ROOT_DIR, topdown=True):
+        if dir_name != ROOT_DIR and not dir_name.startswith("./__"):
+            for fname in file_list:
+                upload_file("%s/" % dir_name[2:], "%s/%s" % (dir_name, fname), fname)
 
-for dir_name, subdir_list, file_list in os.walk(ROOT_DIR, topdown=True):
-    if dir_name != ROOT_DIR:
-        for fname in file_list:
-            upload_file("%s/" % dir_name[2:], "%s/%s" % (dir_name, fname), fname)
+    print("done")
 
 
-print("done")
+if __name__ == '__main__':
+    s3 = boto3.client("s3")
+    bucket_config = {'LocationConstraint': 'ap-southeast-2'}
+    if('-i' in sys.argv or '--initialise=True' in sys.argv):
+        create_bucket(bucket_config)
+        upload_files()
+    else:
+        print(s3.delete_object(Bucket=ROOT_S3_DIR, Key='rootfile.txt'))
+        print(s3.delete_object(Bucket=ROOT_S3_DIR, Key='subfile.txt'))
+        print(s3.delete_bucket(Bucket=ROOT_S3_DIR))
